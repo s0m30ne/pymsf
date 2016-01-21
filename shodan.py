fd8f40bca4b5121c7d78615a7562bb73ed1accec
s0m30ne <s0m30ne@163.com>
2016-01-21 21:27:55 +0800

    python msfconsole

--- shodan.py
+++ shodan.py
@@ -2,15 +2,13 @@ import sys
 import os
 import json
 import requests
-import time
-import math
 from Queue import Queue
+import re
 
-class Censys(object):
+class Shodan(object):
     def __init__(self):
-        self.API_URL = "https://www.censys.io/api/v1"
-        self.UID = None
-        self.SECRET = None
+        self.API_URL = "https://api.shodan.io/shodan/host/search"
+        self.API_KEY = None
 
     def getConfig(self):
         if os.path.exists("config.txt"):
@@ -20,47 +18,36 @@ class Censys(object):
                 if "=" in eachLine:
                     eachConfig = eachLine.split("=")
                     config[eachConfig[0].strip()] = eachConfig[1].strip()
-            self.UID = config['UID']
-            self.SECRET = config['SECRET']
+            self.API_KEY = config['API_KEY']
         else:
-            print "you must config your UID and SECRET for the first time"
+            print "you must config your API_KEY for the first time"
             sys.exit()
 
     def searchIP(self, query, pages, queue, STOP_ME):
-        if self.UID == None or self.SECRET == None:
-            print "please config your UID and SECRET"
+        if self.API_KEY == None:
+            print "please config your API_KEY"
             sys.exit()
         for page in range(1, pages+1):
-            start_time = time.time()
-            data = {
-                "query":query, 
-                "page":page, 
-                "fields":["ip", "autonomous_system.name"]
-            }
             try:
-                res = requests.post(self.API_URL + "/search/ipv4", data=json.dumps(data), auth=(self.UID, self.SECRET))
+                res = requests.get("%s?key=%s&query=%s&page=%s" % (self.API_URL, self.API_KEY, query, page))
             except:
                 print "request error"
                 continue
             else:
                 try:
                     results = res.json
+                    if not isinstance(results, dict):
+                        results = res.json()
                 except:
                     print "json decode error !"
                     continue
                 else:
-                    if res.status_code != 200:
+                    if results.has_key("error"):
                         print "error occurred: %s" % results["error"]
                         sys.exit(1)
                     else:
-                        result_iter = iter(results["results"])
+                        result_iter = iter(results["matches"])
                         for result in result_iter:
-                            queue.put(result["ip"])
-            end_time = time.time()
-            used_time = end_time - start_time
-            if used_time > 2.5:
-                pass
-            else:
-                time.sleep(math.ceil(used_time))
-        
+                            if re.search(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", result["ip_str"]):
+                                queue.put(result["ip_str"])
         STOP_ME[0] = True
